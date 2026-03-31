0875efc4e202fdfab9f986d5b1aec4cb99f80e51
Robert Schütz <nix@dotlambda.de>
2026-03-31 08:32:11 -0700

    python3Packages.zha: 1.0.2 -> 1.1.0
    Diff: https://github.com/zigpy/zha/compare/1.0.2...1.1.0
    
    Changelog: https://github.com/zigpy/zha/releases/tag/1.1.0

--- pkgs/development/python-modules/zha/default.nix
+++ pkgs/development/python-modules/zha/default.nix
@@ -23,7 +23,7 @@
 
 buildPythonPackage rec {
   pname = "zha";
-  version = "1.0.2";
+  version = "1.1.0";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -32,7 +32,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zha";
     tag = version;
-    hash = "sha256-ps3Qq1iGB9uPXe8CWDGLbST90EIAmszT0nuh0AvUjKg=";
+    hash = "sha256-ye56pdQx91fchUBez/CvD0e3gi+HRjZQ2QgtpOLVc9A=";
   };
 
   postPatch = ''
